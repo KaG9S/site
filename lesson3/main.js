bf7fe9fc24a4176a77a70764b8ffae59c3f932a3
George Kanivetc <150721213+KaG9S@users.noreply.github.com>
2024-10-19 15:08:10 +0300

    Add files via upload

---
+++ lesson3/main.js
@@ -0,0 +1,71 @@
+/*let und
+alert(und) //undefined
+
+let nul = null
+alert(nul)
+
+let num1 = 345
+alert(num1)
+let num2 = -23
+alert(num2)
+let num3 = 36.6
+alert(num3)
+
+let str1 = 'hello'
+alert(str1)
+let str2 = "hey"
+alert(str2)
+let str3 = `huh`
+alert(str3)
+
+//error
+//alert('huh' * 2)
+
+let vir1 = 1 + 2
+alert(vir1)//3
+let vir2 = 1 - 2
+alert(vir2)//-1
+let vir3 = 1 * 2
+alert(vir3)//2
+let vir4 = 1 / 2
+alert(vir4)//0.5
+let vir5 = 2 + 8 * 9
+alert(vir5)//74
+let vir6 = (2 + 8) * 9
+alert(vir6)//90
+let vir7 = 6 % 2
+alert(vir7)//0
+let vir8 = 21 % 5
+alert(vir8)//1
+let vir9 = 11 % 6
+alert(vir9)//5
+
+let inf = 1/0
+alert(inf) //infinity
+
+let bool1 = true
+alert(bool1)
+let bool2 = false
+alert(bool2)
+
+let s1 = '8' / '2'
+alert(s1)//4
+let s2 = 3 + '4'
+alert(s2)//34
+
+let s3 = 1
+alert(++s3)//2 було 1 стало 2
+alert(s3++)//2 було 2 стало 3
+//2 тому що s3++ повертає старе значення
+alert(--s3)//2 було 3 стало 2
+alert(s3--)//2 було 2 стало 1
+//2 тому що s3-- повертає старе значення
+
+let s4 = 1
+s4 *= 2
+alert(s4)//2
+s4 *= 3
+alert(s4)//6
+s4 += 2
+alert(s4)//8
+//*/

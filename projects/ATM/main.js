0b03b37a47730232a8b469836a7f4912f1d6ff02
George Kanivetc <150721213+KaG9S@users.noreply.github.com>
2024-12-14 19:20:08 +0200

    .

---
+++ projects/ATM/main.js
@@ -0,0 +1,27 @@
+const input = document.getElementById('input')
+const result = document.getElementById('resultP')
+const resultDiv = document.getElementById('resultDiv')
+function getMoney(){
+    let money = input.value
+    if(money % 1 == 0 && money >= 10){
+        if(money % 10 == 0){
+            result.innerText = 'Операція успішна! Ось ваші '+money+'₴'
+            for(let i = 0; i <= (money - (money % 10)); i++){
+                resultDiv.innerHTML += '<img src="гривня.jpg" alt="1 гривня">'
+            }
+        }else{
+            let rand = Math.floor(Math.random()*100)
+            result.innerText = 'Операція призупинена! Наразі у банкоматі немає купюр, які менше 10₴. Ми можемо віддати вам '+(money - (money % 10))+'₴ або повернути гроші.'
+            if(prompt('Напишіть число '+rand+' якщо хочете повернути кошти. В інакшому разі кошти будуть списані')){
+                result.innerText = 'Операція успішна! Ось ваші '+(money - (money % 10))+'₴'
+                for(let i = 0; i <= (money - (money % 10)); i++){
+                    resultDiv.innerHTML += '<img src="гривня.jpg" alt="1 гривня" style="border: 3px black solid;">'
+                }
+            }else{
+                result.innerText = 'Операція відхилена! Ваші кошти на картці'
+            }
+        }
+    }else{
+        result.innerText = 'Ми не можемо видати вам таку сумму ('+money+'₴)'
+    }
+}
--- projects/hero/main.js
+++ projects/hero/main.js
@@ -1,11 +1,3 @@
-document.write('<div id="main">\
-    <h1>Вгадай героя!</h1>\
-    <p>Цей герой - детектив. У нього є помічник Ватсон. Він розкрив дуже багато злочинів</p>\
-    <p>Цей герой:</p>\
-    <input id="input"></input>\
-    <button onclick="result()">Перевірити</button>\
-    <p id="resultP"></p>\
-    ')
 const answers = ['Шерлок','Шерлок Холмс','Холмс Шерлок']
 function result(){
     let answer = document.getElementById('input').value

dad32c46f1939195220925f290832f36861ebfd7
George Kanivetc <150721213+KaG9S@users.noreply.github.com>
2024-12-14 20:03:14 +0200

    aaaaaaaaaaa

--- projects/ATM/main.js
+++ projects/ATM/main.js
@@ -8,15 +8,15 @@ function getMoney(){
     if(money % 1 == 0 && money >= 10){
         if(money % 10 == 0){
             result.innerText = 'Операція успішна! Ось ваші '+money+'₴'
-            for(let i = 0; i <= (money - (money % 10)); i++){
+            for(let i = 0; i < (money - (money % 10)); i++){
                 resultDiv.innerHTML += '<img src="гривня.jpg" alt="1 гривня">'
             }
         }else{
             let rand = Math.floor(Math.random()*100)
             if((prompt('Операція призупинена! Наразі у банкоматі немає купюр, які менше 10₴. Ми можемо віддати вам '+(money - (money % 10))+'₴ або повернути гроші. Напишіть число '+rand+' якщо хочете повернути кошти. В інакшому разі кошти будуть списані'))){
                 result.innerText = 'Операція успішна! Ось ваші '+(money - (money % 10))+'₴'
-                for(let i = 0; i <= (money - (money % 10)); i++){
-                    resultDiv.innerHTML += '<img src="гривня.jpg" alt="1 гривня" style="border: 3px black solid;" wigth="'+(wigth/100)+'" height="'+(height/100)+'">'
+                for(let i = 0; i < (money - (money % 10)); i++){
+                    resultDiv.innerHTML += '<img src="гривня.jpg" alt="1 гривня" style="border: 3px black solid;" wigth="'+(wigth/100)+'">'
                 }
             }else{
                 result.innerText = 'Операція відхилена! Ваші кошти на картці'

5092a11d4f7f48b48ffcb2eae2f6ec5a936e197b
George Kanivetc <150721213+KaG9S@users.noreply.github.com>
2024-12-14 19:57:27 +0200

    .

--- projects/ATM/main.js
+++ projects/ATM/main.js
@@ -1,6 +1,8 @@
 const input = document.getElementById('input')
 const result = document.getElementById('resultP')
 const resultDiv = document.getElementById('resultDiv')
+const wigth = 8315
+const height = 4444
 function getMoney(){
     let money = input.value
     if(money % 1 == 0 && money >= 10){
@@ -14,7 +16,7 @@ function getMoney(){
             if((prompt('Операція призупинена! Наразі у банкоматі немає купюр, які менше 10₴. Ми можемо віддати вам '+(money - (money % 10))+'₴ або повернути гроші. Напишіть число '+rand+' якщо хочете повернути кошти. В інакшому разі кошти будуть списані'))){
                 result.innerText = 'Операція успішна! Ось ваші '+(money - (money % 10))+'₴'
                 for(let i = 0; i <= (money - (money % 10)); i++){
-                    resultDiv.innerHTML += '<img src="гривня.jpg" alt="1 гривня" style="border: 3px black solid;">'
+                    resultDiv.innerHTML += '<img src="гривня.jpg" alt="1 гривня" style="border: 3px black solid;" wigth="'+(wigth/100)+'" height="'+(height/100)+'">'
                 }
             }else{
                 result.innerText = 'Операція відхилена! Ваші кошти на картці'
